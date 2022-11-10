a044a3066b29892b504e90fbeff5a3da65935020
Israel Adegoke <117425865+ezzisrael@users.noreply.github.com>
2022-11-10 21:11:16 +0100

    Update tutorial.java

--- tutorial.java
+++ tutorial.java
@@ -1,8 +1,35 @@
-package com.javautorial;
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.Date;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int ages = 30, temp = 20;
+        int tte = 20;
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+        System.out.println("Hello world!");
+        //types in java
+        // byte = 1 //short = 2
+        //int = 4 //long = 8
+        //float 4 for decimal numbers //double = 8
+        // char = 2 //boolean = 2
+        //int viewsCount = 123_456_789; //long viewsCounts = 3_123_456_789L;
+        //float price = 10.99F; //char letter = 'A'; // ='' for single charater
+        //boolean isEligible = false;
+       // byte age = 30;
+        //Date now = new Date();
+        //System.out.println(now);
+        //byte x = 1;
+        //byte y = x;
+        //x = 2;
+        //System.out.println(y);
+
+        //ref types
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
