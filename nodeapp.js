6564c1e2e298440ef17d99b1a5d137d360ecfa49
Israel Adegoke <117425865+ezzisrael@users.noreply.github.com>
2022-11-10 08:16:35 +0100

    Update nodeapp.js

--- nodeapp.js
+++ nodeapp.js
@@ -3,5 +3,22 @@ function sayHello(name) {
     console.log('Hello ' + name);
 }
 
-//sayHello('Mosh');
-console.log(window);
+sayHello('Mosh');
+
+console.log(); // global
+
+setTimeout()
+clearTimeout()
+
+setInterval()
+clearInterval()
+
+var message = '';
+global.setTimeout
+window.setTimeout
+
+var sayHello = function() {
+
+}
+
+window.sayHello(); 
