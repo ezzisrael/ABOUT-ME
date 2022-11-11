93b6ed36c270394638a677811df001e649748ce2
Israel Adegoke <117425865+ezzisrael@users.noreply.github.com>
2022-11-12 00:06:03 +0100

    Create Python Test.py

---
+++ Python Test.py
@@ -0,0 +1,15 @@
+#SIMPLE SIGNING AND LOGIN.
+print('Sign-up Here')
+Username = input('Please Enter Username: ')
+Password = input('Please Enter Password: ')
+print('Signing-Up Succesafull')
+print('\n')
+print('Please Log-In.')
+username = input('Please Enter Your Username: ')
+password = input('Please Enter Your Password: ')
+
+if Username == username and Password == password:
+    print('Log-in Successful.')
+else:
+    print('Incorrect Details, Please\nPlease Enter Correct Details')
+print('LOGING IN....')
