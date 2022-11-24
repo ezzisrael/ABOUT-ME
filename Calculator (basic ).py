7b2cbed9503c4c275844e62983f87f58dc78544c
Israel Adegoke <117425865+ezzisrael@users.noreply.github.com>
2022-11-24 11:21:09 +0100

    Update Calculator (basic ).py
    with tkinter

--- Calculator (basic ).py
+++ Calculator (basic ).py
@@ -54,3 +54,71 @@ while True:    #to keep the opration in loop til the quit botton is used.
     elif choice == 'e' or choice == 'E':
         print('Progrom don finish')
         quit()
+        
+        
+        
+        
+        
+        
+#TRY this with tkinter.
+
+import tkinter
+from tkinter import *
+
+root=Tk()
+root.title('simple Calculator')
+root.geometry('500x600+100+200')
+root.resizable(0,0)
+root.configure(bg='#17161b')
+
+equation=""
+
+def show(value):
+    global equation
+    equation+=value
+    label_result.config(text=equation)
+
+def clear():
+    global equation
+    equation= ""
+    label_result.config(text=equation)
+
+def calculate():
+    global equation
+    result= ""
+    if equation !="":
+        try:
+            result= eval(equation)
+        except:
+            result= "error"
+            equation= ""
+    label_result.config(text=result)
+
+label_result=Label(root, width=25,height=2, text='',font=('arial',30))
+label_result.pack()
+
+Button(root, text='c', width=5, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='#3697f5', command=lambda: clear()).place(x=10,y=100)
+Button(root, text='/', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('/')).place(x=150,y=100)
+Button(root, text='%', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray' , command=lambda: show('%')).place(x=268,y=100)
+Button(root, text='*', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('*')).place(x=384,y=100)
+
+Button(root, text='7', width=5, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('7')).place(x=10,y=200)
+Button(root, text='8', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('8')).place(x=150,y=200)
+Button(root, text='9', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('9')).place(x=268,y=200)
+Button(root, text='-', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('-')).place(x=384,y=200)
+
+Button(root, text='4', width=5, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('4')).place(x=10,y=300)
+Button(root, text='5', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('5')).place(x=150,y=300)
+Button(root, text='6', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('6')).place(x=268,y=300)
+Button(root, text='+', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('+')).place(x=384,y=300)
+
+Button(root, text='1', width=5, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('1')).place(x=10,y=400)
+Button(root, text='2', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('2')).place(x=150,y=400)
+Button(root, text='3', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('3')).place(x=268,y=400)
+Button(root, text='=', width=4, height=3, font=('arial',30,'bold',), bd=1,fg='#fff', bg='yellow', command=lambda: calculate()).place(x=384,y=400)
+
+
+Button(root, text='0', width=10, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('0')).place(x=10,y=500)
+Button(root, text='.', width=4, height=1, font=('arial',30,'bold',), bd=1,fg='#fff', bg='gray', command=lambda: show('.')).place(x=268,y=500)
+
+root.mainloop()
